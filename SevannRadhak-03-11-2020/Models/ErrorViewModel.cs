42d2010596b838f5cc7023d9866db6783305f1a6
Sevann Radhak <sevann.radhak@gmail.com>
2020-11-03 14:16:37 -0300

    First commit: create structrue project

---
+++ SevannRadhak-03-11-2020/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SevannRadhak_03_11_2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
